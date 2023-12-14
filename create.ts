3101444283f4e8abbd77d59b9005e95c837dad70
Marsot Pierre <pierre.mmarsot@gmail.com>
2023-12-14 12:36:05 +0100

    hub

--- create.ts
+++ create.ts
@@ -247,6 +247,7 @@ const vote = async (gauges: string[], proposalId: string) => {
 const main = async () => {
 
   const hub = process.env.HUB;
+  console.log("hub", hub);
 
   const client = new snapshot.Client712(hub);
   const pk: BytesLike = process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : "";
