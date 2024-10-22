12bd75c6cffb8ae528b6adcbcd6879d1b132f314
Laouni <99427751+Lao0ni@users.noreply.github.com>
2024-10-22 17:22:12 +0200

    :adhesive_bandage: fix: using custom rpcs

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,1 +1,10 @@
 export const ANGLE_ONCHAIN_SUBGRAPH_URL = "https://api.goldsky.com/api/public/project_cltpyx1eh5g5v01xi0a5h5xea/subgraphs/governance-eth/prod/gn"
+
+export const CHAIN_ID_TO_RPC = {
+    1: "https://eth-mainnet.g.alchemy.com/v2/"+process.env.WEB3_ALCHEMY_API_KEY,
+    56: "https://sleek-solitary-mansion.bsc.quiknode.pro/b818cf0343b83e2a6eecc966b96a4002b474e624/",
+    42161: "https://arb-mainnet.g.alchemy.com/v2/"+process.env.WEB3_ALCHEMY_API_KEY,
+    8453: "https://base-mainnet.g.alchemy.com/v2/"+process.env.WEB3_ALCHEMY_API_KEY,
+    10: "https://opt-mainnet.g.alchemy.com/v2/"+process.env.WEB3_ALCHEMY_API_KEY,
+    137: "https://polygon-mainnet.g.alchemy.com/v2/"+process.env.WEB3_ALCHEMY_API_KEY
+}
