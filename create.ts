29fb102a081c32dcdbcea91ae1d6695e7e033c0f
Marsot Pierre <pierre.mmarsot@gmail.com>
2024-12-19 09:44:41 +0100

    main

--- create.ts
+++ create.ts
@@ -1016,7 +1016,7 @@ const votes = async () => {
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-votes().catch((error) => {
+main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
