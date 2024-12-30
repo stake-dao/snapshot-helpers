ce338052fb2d4e92ffb0374512b8354fe2607258
Lao0ni <99427751+Lao0ni@users.noreply.github.com>
2024-12-30 21:18:43 +0100

    fix: reverting change

--- mirror.ts
+++ mirror.ts
@@ -446,7 +446,7 @@ const handleCurve = async () => {
     const sdResult = await fetchSDProposal({ space: SDCRVGOV });
     const sdProposals = filterGaugesProposals(sdResult);
     const minCreated = sdProposals?.[0]?.created;
-    const result = await fetchCurveProposalWithCurveMonitor(1735395754);
+    const result = await fetchCurveProposalWithCurveMonitor(minCreated);
 
     for (const data of result) {
         const link = `https://dao.curve.fi/vote/${data.vote_type.toLowerCase()}/${data.vote_id}`;
