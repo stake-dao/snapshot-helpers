c09b9669f80b6cdc47b0fcc10d6032e0dea38dcc
Marsot Pierre <pierre.mmarsot@gmail.com>
2025-05-06 11:46:53 +0200

    upgrade viem

--- utils/constants.ts
+++ utils/constants.ts
@@ -11,7 +11,8 @@ export const CHAIN_ID_TO_RPC = {
     42161: "https://arb-mainnet.g.alchemy.com/v2/" + process.env.WEB3_ALCHEMY_API_KEY,
     8453: "https://base-mainnet.g.alchemy.com/v2/" + process.env.WEB3_ALCHEMY_API_KEY,
     10: "https://opt-mainnet.g.alchemy.com/v2/" + process.env.WEB3_ALCHEMY_API_KEY,
-    137: "https://polygon-mainnet.g.alchemy.com/v2/" + process.env.WEB3_ALCHEMY_API_KEY
+    137: "https://polygon-mainnet.g.alchemy.com/v2/" + process.env.WEB3_ALCHEMY_API_KEY,
+    146: "https://sonic-mainnet.g.alchemy.com/v2/" + process.env.WEB3_ALCHEMY_API_KEY,
 }
 
 export const MS_ADDRESS = "0xB0552b6860CE5C0202976Db056b5e3Cc4f9CC765";
