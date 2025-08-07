cf575f450bf0f9fa1f0b35404ac8b4949138680e
Marsot Pierre <pierre.mmarsot@gmail.com>
2025-08-07 10:38:42 +0200

    vote pendle

--- src/gauge-proposals/pendle.ts
+++ src/gauge-proposals/pendle.ts
@@ -2,7 +2,9 @@ import { CreateProposal } from "./createProposal";
 import axios from "axios";
 import moment from "moment";
 import { sleep } from "../../utils/sleep";
-import { exit } from "process";
+import { CHAT_ID_ERROR, sendMessage } from "../../utils/telegram";
+import snapshot from "@snapshot-labs/snapshot.js";
+import { BytesLike, ethers } from "ethers";
 
 class PendleCreateProposal extends CreateProposal {
 
@@ -35,7 +37,7 @@ class PendleCreateProposal extends CreateProposal {
         const SIZE = 100;
         const response: string[] = [];
 
-        const {data: {chainIds}} = await axios.get("https://api-v2.pendle.finance/core/v1/chains")
+        const { data: { chainIds } } = await axios.get("https://api-v2.pendle.finance/core/v1/chains")
 
         for (const chainId of chainIds) {
             let run = true;
@@ -79,6 +81,56 @@ class PendleCreateProposal extends CreateProposal {
 
         return response;
     }
+
+    
+    public async vote(receipt: any, gauges: string[], waitSleep?: boolean): Promise<void> {
+        // Wait 5 minutes to be in the voting window
+        await sleep(1 * 60 * 1000);
+
+        // Push a vote on mainnet from PK for asdPendle
+        await this.votePendle(gauges, receipt.id as string, process.env.VOTE_PRIVATE_KEY, "0xfa19d3a9F73180c9E73D2811e0b66EEED612f728");
+    }
+
+    private async votePendle(gauges: string[], proposalId: string, pkStr: string, targetGaugeAddress: string) {
+
+        let choiceIndex = -1;
+        for (let i = 0; i < gauges.length; i++) {
+            const gauge = gauges[i];
+
+            if (gauge.toLowerCase().indexOf(targetGaugeAddress.toLowerCase()) > -1) {
+                choiceIndex = i;
+                break;
+            }
+        }
+
+        if (choiceIndex === -1) {
+            const msg = `Impossible to find target gauge. Proposal id : ${proposalId} / target gauge address : ${targetGaugeAddress}`;
+            console.log(msg);
+            await sendMessage(process.env.TG_API_KEY_BOT_ERROR, CHAT_ID_ERROR, `Create weekly proposal`, msg);
+            return;
+        }
+
+        const hub = process.env.HUB;
+        const client = new snapshot.Client712(hub);
+        const pk: BytesLike = pkStr;
+        const web3 = new ethers.Wallet(pk);
+
+        const choice: any = {};
+        choice[(choiceIndex + 1).toString()] = 1;
+
+        try {
+            await client.vote(web3 as any, web3.address, {
+                space: this.getSpace(),
+                proposal: proposalId,
+                type: 'weighted',
+                choice,
+            });
+        }
+        catch (e) {
+            console.log(e);
+            await sendMessage(process.env.TG_API_KEY_BOT_ERROR, CHAT_ID_ERROR, `Create weekly proposal`, `Can't vote for PENDLE proposal - ${e.error_description || e.message || ""}`);
+        }
+    };
 }
 
 new PendleCreateProposal().job();
